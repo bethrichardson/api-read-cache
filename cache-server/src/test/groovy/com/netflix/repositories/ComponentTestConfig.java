13913d48b05c70e967e79538941373111ba4f94d
Beth Richardson <bethlrichardson@gmail.com>
2021-02-26 14:05:01 -0600

    Add endpoint to pull fork metrics from GitHub for an org
    - GitHubClient from spotify used with Personal Access Token passed
    as env variable
    - No caching and errors are swallowed
    - Repositories are not sorted or returned based on top metrics
    - Format is JSON now; will change later to match requirements

--- cache-server/src/test/groovy/com/netflix/repositories/ComponentTestConfig.java
+++ cache-server/src/test/groovy/com/netflix/repositories/ComponentTestConfig.java
@@ -1,11 +1,16 @@
 package com.netflix.repositories;
 
 import com.netflix.repositories.config.RepositoryMetricsConfig;
+import com.spotify.github.v3.clients.GitHubClient;
+import com.spotify.github.v3.clients.RepositoryClient;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import spock.mock.DetachedMockFactory;
 
+@SuppressWarnings("PMD.BeanMembersShouldSerialize")
 @Configuration
 @EnableAutoConfiguration
 @Import({
@@ -15,5 +20,17 @@ import spock.mock.DetachedMockFactory;
 public class ComponentTestConfig {
 
     private final DetachedMockFactory mockFactory = new DetachedMockFactory();
+
+    @Bean
+    @Primary
+    public GitHubClient mockGitHubClient() {
+        return mockFactory.Mock(GitHubClient.class);
+    }
+
+    @Bean
+    @Primary
+    public RepositoryClient mockRepoClient() {
+        return mockFactory.Mock(RepositoryClient.class);
+    }
 
 }
--- cache-server/src/test/groovy/com/netflix/repositories/RepositoryMetricsSpec.groovy
+++ cache-server/src/test/groovy/com/netflix/repositories/RepositoryMetricsSpec.groovy
@@ -2,24 +2,51 @@ package com.netflix.repositories
 
 import com.netflix.repositories.client.RepositoryMetricsClient
 import com.netflix.repositories.common.RepositoryMetric
+import com.spotify.github.v3.clients.GitHubClient
+import com.spotify.github.v3.clients.RepositoryClient
+import com.spotify.github.v3.repos.Repository
 import org.springframework.beans.factory.annotation.Autowired
 import spock.lang.Specification
+
+import java.util.concurrent.CompletableFuture
 
 @ComponentTest
 class RepositoryMetricsSpec extends Specification  {
 
     @Autowired
-    RepositoryMetricsClient client
+    RepositoryMetricsClient repositoryMetricsClient
+
+    @Autowired
+    RepositoryClient gitHubClient
 
     def "should call out to get a list of Netflix repos"() {
         given:
         int numberRepos = 5
+        List<Repository> expectedList = []
+        10.times {
+            Repository repository = Mock(Repository)
+            repository.forksCount() >> it
+            expectedList.add(repository)
+        }
 
         when:
-        List<List<RepositoryMetric>> list = client.getTopRepositoriesByForks(numberRepos)
+        List<RepositoryMetric> actualList = repositoryMetricsClient.getTopRepositoriesByForks(numberRepos)
 
         then:
-        list.isEmpty()
+        1 * gitHubClient.listOrganizationRepositories() >> CompletableFuture.completedFuture(expectedList)
+        assert actualList.size() == numberRepos
+    }
+
+    def "should return an empty list if result is not available"() {
+        given:
+        int numberRepos = 5
+
+        when:
+        List<RepositoryMetric> actualList = repositoryMetricsClient.getTopRepositoriesByForks(numberRepos)
+
+        then:
+        1 * gitHubClient.listOrganizationRepositories() >> { throw new InterruptedException() }
+        assert actualList.isEmpty()
     }
 
 }
