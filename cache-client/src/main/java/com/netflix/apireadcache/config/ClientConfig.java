d931cec30562540d97970dd59d7351b99c684984
Beth Richardson <bethlrichardson@gmail.com>
2021-02-28 18:46:46 -0600

    Add config for client port and url

--- cache-client/src/main/java/com/netflix/apireadcache/config/ClientConfig.java
+++ cache-client/src/main/java/com/netflix/apireadcache/config/ClientConfig.java
@@ -2,17 +2,23 @@ package com.netflix.apireadcache.config;
 
 import com.netflix.apireadcache.client.ApiReadCache;
 import feign.Feign;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientConfig {
 
+    @Value("${server.port}")
+    String serverPort;
+
+    @Value("${server.url}")
+    String baseUrl;
+
     @Bean
     public ApiReadCache repositoryMetricsClient() {
         return Feign.builder()
                 .decoder(new JsonOrTextDecoder())
-                .target(ApiReadCache.class, "http://localhost:10000"); // TODO: configurable ports
+                .target(ApiReadCache.class, baseUrl + ":" + serverPort);
     }
-
 }
